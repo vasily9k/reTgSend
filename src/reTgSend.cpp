d2a19de32befc17ab7f38c9e0ef6e0fa1d18cfe7
rs.pink <rs.pink@gmail.com>
2024-09-22 03:05:13 +0800

    minor

--- src/reTgSend.cpp
+++ src/reTgSend.cpp
@@ -786,7 +786,7 @@ bool tgSendLinkToFile(cJSON *result)
       cJSON *file_size = cJSON_GetObjectItemCaseSensitive(result, "file_size");
       static const char fmt[] = "Получен файл №%d, размером %d байт.\n"
                          "Для обновления прошивки выполните команду\n"
-                         "/upgrade_%d\n";
+                         "/upgrade_%d";
       int f_sz = file_size->valuedouble ? file_size->valuedouble : 0;
 
       int sz = snprintf(NULL, 0, fmt, file_number, f_sz, file_number);
